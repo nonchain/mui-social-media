09c50c1dbdd13fbc8599328cd67e7e50abc2ae76
nonchain <code.progali@gamil.com>
2023-01-15 13:57:06 +0330

    Add more post

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -128,6 +128,117 @@ function Feed() {
                </IconButton>
             </CardActions>
          </Card>
+         <Card>
+            <CardHeader
+               avatar={
+                  <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+                     R
+                  </Avatar>
+               }
+               action={
+                  <IconButton aria-label="settings">
+                     <MoreVert />
+                  </IconButton>
+               }
+               title="Funny food for weekend"
+               subheader="September 14, 2022"
+            />
+            <CardMedia
+               component="img"
+               height="198"
+               image="https://i1.delgarm.com//images/news/a798/1398/09/06/b/1574839454_A8yJ4.jpg"
+               alt="Paella dish"
+            />
+            <CardContent>
+               <Typography variant="body2" color="text.secondary">
+                  This impressive paella is a perfect party dish and a fun meal to cook
+                  together with your guests. Add 1 cup of frozen peas along with the mussels,
+                  if you like.
+               </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+               <IconButton aria-label="add to favorites">
+                  <Favorite />
+               </IconButton>
+               <IconButton aria-label="share">
+                  <Share />
+               </IconButton>
+            </CardActions>
+         </Card>
+         <Card>
+            <CardHeader
+               avatar={
+                  <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+                     R
+                  </Avatar>
+               }
+               action={
+                  <IconButton aria-label="settings">
+                     <MoreVert />
+                  </IconButton>
+               }
+               title="Alone tree"
+               subheader="AUgust 04, 2022"
+            />
+            <CardMedia
+               component="img"
+               height="198"
+               image="https://cdn.mashreghnews.ir/d/2020/02/16/4/2718373.jpg"
+               alt="Paella dish"
+            />
+            <CardContent>
+               <Typography variant="body2" color="text.secondary">
+                  This impressive paella is a perfect party dish and a fun meal to cook
+                  together with your guests. Add 1 cup of frozen peas along with the mussels,
+                  if you like.
+               </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+               <IconButton aria-label="add to favorites">
+                  <Favorite />
+               </IconButton>
+               <IconButton aria-label="share">
+                  <Share />
+               </IconButton>
+            </CardActions>
+         </Card>
+         <Card>
+            <CardHeader
+               avatar={
+                  <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+                     R
+                  </Avatar>
+               }
+               action={
+                  <IconButton aria-label="settings">
+                     <MoreVert />
+                  </IconButton>
+               }
+               title="Lamborghini"
+               subheader="January 09, 2022"
+            />
+            <CardMedia
+               component="img"
+               height="198"
+               image="https://setare.com/files/fa/news/1399/4/29/371883_797.jpg"
+               alt="Paella dish"
+            />
+            <CardContent>
+               <Typography variant="body2" color="text.secondary">
+                  This impressive paella is a perfect party dish and a fun meal to cook
+                  together with your guests. Add 1 cup of frozen peas along with the mussels,
+                  if you like.
+               </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+               <IconButton aria-label="add to favorites">
+                  <Favorite />
+               </IconButton>
+               <IconButton aria-label="share">
+                  <Share />
+               </IconButton>
+            </CardActions>
+         </Card>
       </Box>
    )
 }
